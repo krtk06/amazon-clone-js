e35fc57836b2c1afb188190dd21c7332f7d11400
krtk06 <krtk2805@gmail.com>
2025-05-30 17:56:19 +0530

    Initial Commit

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,36 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        stars:4.5,
-        count:'87'
-    },
-    price:' 4090'
-},{
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating:{
-        stars:4,
-        count:'127'
-    },
-    price:'2095'
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars:4.5,
-        count:'56'
-    },
-    price:'7990'
-}];
-
+// const products = [{
+//     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//     rating:{
+//         stars:4.5,
+//         count:'87'
+//     },
+//     price:' 4090'
+// },{
+//     image: 'images/products/intermediate-composite-basketball.jpg',
+//     name: 'Intermediate Size Basketball',
+//     rating:{
+//         stars:4,
+//         count:'127'
+//     },
+//     price:'2095'
+// },{
+//     image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
+//     rating:{
+//         stars:4.5,
+//         count:'56'
+//     },
+//     price:'7990'
+// },{
+//   image:'images/products/black-2-slot-toaster.jpg',
+//   name: '2-Slot toaster-Black',
+//     rating:{
+//         stars:5,
+//         count:2197
+//     },
+//     price:'18990'
+// }];
 let productHTML = '';
 products.forEach((product) =>{
     productHTML += `
@@ -46,7 +53,7 @@ products.forEach((product) =>{
           </div>
 
           <div class="product-price">
-            RS.${(product.price/10).toFixed(2)}
+            RS.${(product.price).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
